58b44ba18efd054466ebb5a39e9d398b6bb6feda
Mujeeb ur Rehman <mbrehman90@gmail.com>
2025-07-13 18:07:41 +0000

    Strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,41 @@
+const name ="Mujeeb ur Rehman"
+const age = 26
+
+console.log("name" + name + "and age is" + age);
+
+// console.log(`Name is ${name} and age is ${age}`);
+
+console.log(`Name is ${name} and age is ${age}`);
+
+
+
+const city = new String ('     Islamabad-htc-kjih-popubhjbj   ')
+
+// console.log(city.anchor());
+// console.log(city.toLocaleUpperCase());
+
+console.log(city.slice(0,4));  // Breaks the string values and we can use negative values as well
+
+const newString = city.substring(0,5) // Breaks the sting but we can't use negative values
+
+console.log(newString);
+
+
+
+const newstring1 = city;
+
+
+console.log (newstring1);
+
+console.log (newstring1.trim());
+
+
+console.log(city.split('-'));
+
+
+// String Function needs to be check throughly
+
+
+
+
+
